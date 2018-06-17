7f7121eab9ee900a32dffc0d2de2955a4a10fcf0
AymanElakhwas <aelakhwas@mum.edu>
2018-06-17 06:03:54 -0500

    add missing dependencies for material ui

--- hunger-station-angular/src/app/app.module.ts
+++ hunger-station-angular/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CutomerModule } from './modules/cutomer/cutomer.module'
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CutomerModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- hunger-station-angular/src/app/modules/cutomer/cutomer.module.ts
+++ hunger-station-angular/src/app/modules/cutomer/cutomer.module.ts
@@ -4,12 +4,20 @@ import { CommonModule } from '@angular/common';
 import { CutomerRoutingModule } from './cutomer-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { OrderBasketComponent } from './components/order-basket/order-basket.component';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule, MatInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     CommonModule,
-    CutomerRoutingModule
+    CutomerRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatInputModule
   ],
-  declarations: [LoginComponent, OrderBasketComponent]
+  declarations: [LoginComponent, OrderBasketComponent],
+  exports: [LoginComponent, OrderBasketComponent]
 })
 export class CutomerModule { }

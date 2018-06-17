e716650ead115d4a7d028c94b481bc25c2a968cb
AymanElakhwas <aelakhwas@mum.edu>
2018-06-17 04:11:10 -0500

    create restaurant module

---
+++ hunger-station-angular/src/app/modules/restaurant/restaurant.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RestaurantRoutingModule } from './restaurant-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RestaurantRoutingModule
+  ],
+  declarations: []
+})
+export class RestaurantModule { }

a3e026fc79d13ee25458f2f7b0e1f4382358f2b1
AymanElakhwas <aelakhwas@mum.edu>
2018-06-17 04:09:11 -0500

    create order-basket component

--- hunger-station-angular/src/app/modules/cutomer/cutomer.module.ts
+++ hunger-station-angular/src/app/modules/cutomer/cutomer.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CutomerRoutingModule } from './cutomer-routing.module';
 import { LoginComponent } from './components/login/login.component';
+import { OrderBasketComponent } from './components/order-basket/order-basket.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CutomerRoutingModule
   ],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent, OrderBasketComponent]
 })
 export class CutomerModule { }

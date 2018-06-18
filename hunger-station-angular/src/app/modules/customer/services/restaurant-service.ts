eda0c39f43f1a94f3538b3599f0a0916edac7e95
Beakal Asefa <beakal.asefa@gmail.com>
2018-06-18 16:55:13 -0500

    restaurant service update

--- hunger-station-angular/src/app/modules/customer/services/restaurant-service.ts
+++ hunger-station-angular/src/app/modules/customer/services/restaurant-service.ts
@@ -11,7 +11,7 @@ export class RestaurantService {
         return this.http.get('http://localhost:3000/restaurants');
     }
 
-    findOne(id:Number) {
+    findOne(id:string) {
         return this.http.get('http://localhost:3000/restaurants/' + id)
     }
 }

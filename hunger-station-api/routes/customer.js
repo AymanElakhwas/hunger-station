f2e574f9406d3e04de10e3f27df7abc53274b312
AymanElakhwas <aelakhwas@mum.edu>
2018-06-17 20:43:06 -0500

    add check for returned customer from the db

--- hunger-station-api/routes/customer.js
+++ hunger-station-api/routes/customer.js
@@ -4,6 +4,7 @@ const Customer = require("../models/customer");
 const jwt = require('jsonwebtoken');
 
 router.post('/auth', (req, res, next) => {
+    console.log(JSON.stringify(req.body));
     Customer.findOne({ 'email': req.body['username'] }, function (err, cus) {
         if (err) {
             console.log(err);
@@ -11,7 +12,7 @@ router.post('/auth', (req, res, next) => {
                 error: err
             });
         }
-        if (cus.password == req.body['password']) {
+        if (cus && cus.password == req.body['password']) {
             const token = jwt.sign(JSON.stringify(cus), 'secret');
             res.json({ 'token': token });
         } else {

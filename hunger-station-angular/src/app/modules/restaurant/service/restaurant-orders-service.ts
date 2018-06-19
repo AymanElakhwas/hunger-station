c23f56b01c72a26e20ce48c16785855007d64b16
AymanElakhwas <aelakhwas@mum.edu>
2018-06-18 23:24:02 -0500

    add updateOrderStatus service

--- hunger-station-angular/src/app/modules/restaurant/service/restaurant-orders-service.ts
+++ hunger-station-angular/src/app/modules/restaurant/service/restaurant-orders-service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
-import{HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class RestaurantOrdersService {
 
   constructor(private http: HttpClient) { }
 
-  getOrdersByRestaurantId(restaurantId){
+  getOrdersByRestaurantId(restaurantId) {
     return this.http.get('http://localhost:3000/api/orders/restaurants/' + restaurantId);
+  }
+
+  updateOrderStatus(order) {
+    return this.http.put('http://localhost:3000/api/orders/' + order.id, order);
   }
 
 
